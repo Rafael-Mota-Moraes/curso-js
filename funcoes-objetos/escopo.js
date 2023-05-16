719c9297250594e7e39e8262b64cf5b7cbb815b1
Rafael Mota Moraes <rafaelmota622@gmail.com>
2023-05-16 10:10:09 -0300

    Escopo léxico

---
+++ funcoes-objetos/escopo.js
@@ -0,0 +1,13 @@
+const idade = 12;
+
+const exibirIdade = () => {
+  const idade = 23;
+  console.log(idade);
+};
+
+const exibirExibirIdade = () => {
+  exibirIdade();
+  console.log(idade);
+};
+
+exibirExibirIdade();
